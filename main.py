909dfca10c714a612a0bc04ae57833b8636ef282
gramkii <147316247+gramkii@users.noreply.github.com>
2023-12-12 18:38:42 +0100

    Add files via upload

--- main.py
+++ main.py
@@ -3,20 +3,21 @@ from PyQt5.QtCore import *
 from PyQt5.QtGui import *
 from random import *
 import time 
-
-app = QApplication([])
+import sys 
+import string
+#app = QApplication([])
 
 #window = QWidget()
 #window.setWindowTitle("Тренування друку")
 #window.resize(700,500)
 
 
-start_test = QPushButton("Почати тест")
-restart_test = QPushButton("Спробувати знову")
+#start_test = QPushButton("Почати тест")
+#restart_test = QPushButton("Спробувати знову")
 
-user_input = QLineEdit()
-word_label = QLabel("")
-result = QLabel()
+#user_input = QLineEdit()
+#word_label = QLabel("")
+#result = QLabel()
 
 class Trainer(QWidget):
     def __init__(self):
@@ -26,9 +27,10 @@ class Trainer(QWidget):
     def init_Gui(self):
         self.start_test = QPushButton("Почати тест", self)
         self.restart_test = QPushButton("Спробувати знову", self)
-
+        self.start_test.clicked.connect(self.start_training)
         self.user_input = QLineEdit(self)
         self.word_label = QLabel("", self)
+        self.word_label.setStyleSheet('font-size: 19px; font-weight: bold;')
         self.result = QLabel("", self)
         layout = QVBoxLayout()
         layout.addWidget(self.start_test)
@@ -36,6 +38,8 @@ class Trainer(QWidget):
         layout.addWidget(self.user_input)
         layout.addWidget(self.word_label)
         layout.addWidget(self.result)
+        self.timer = QTimer(self)
+        self.timer.timeout.connect(self.check)
         self.setLayout(layout)
         self.resize(700, 500)
         self.show()
@@ -44,18 +48,60 @@ class Trainer(QWidget):
         choosed_word = choice(["skoro.txt", "skoro2.txt", "skoro3.txt", "skoro4.txt"])
         with open (choosed_word, "r", encoding="UTF-8") as file:
             word = file.read()
+        print(word)
+
 
         return word
+
+    def generate_random_word(self):
+        return ''.join(choice(string.ascii_lowercase) for _ in range(5))
 
     def start_training(self):
         self.word_type = self.choose_word()
         self.word_label.setText(self.word_type)
         self.user_input.clear()
-        self.result_label.clear()
-        self.start_test(False)
+        self.result.clear()
+        #self.start_test(False)
+        self.timer.start(100)
 
 
-trainer = Trainer()
+
+
+    def check(self):
+        user_input = self.user_input.text()
+        if user_input == self.word_type:
+            self.result.setText("Правильно!")
+            self.timer.stop()
+        else:
+            self.result.clear()
+            
+if __name__ == '__main__':
+    app = QApplication(sys.argv)
+    app.setStyleSheet("""
+    QWidget {
+        background: #337CCF;
+    }
+
+    QPushButton
+    {
+        background: #FFC436;
+        border-width: 7px;
+        border-color: #191D88;
+        border-style: inset;
+        font-family: Arial;
+    }
+    QLabel {
+        font-family: Arial;
+    }
+    QLineEdit {
+        font-family: Arial; 
+        font-size: 16px;
+    }
+""")
+    trainer = Trainer()
+    sys.exit(app.exec_())
+
+#trainer = Trainer()
 
 
 
